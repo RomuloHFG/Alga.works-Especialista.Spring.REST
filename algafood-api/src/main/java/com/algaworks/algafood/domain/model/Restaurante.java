a7cd4b214ef7230d1c4c64c6a5eab7d78a4bd5b7
Rômulo Henrique Ferreira Gonçalves <romulo.henrique.dev@gmail.com>
2024-08-21 00:22:57 -0300

    Add files via upload

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -9,21 +9,23 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Restaurante {
-	
+
 	@Id
-	private Long id;
+	private Long idLong;
 	
 	private String nome;
 	
 	@Column(name = "taxa_frete")
 	private BigDecimal taxaFrete;
 
-	public Long getId() {
-		return id;
+
+
+	public Long getIdLong() {
+		return idLong;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setIdLong(Long idLong) {
+		this.idLong = idLong;
 	}
 
 	public String getNome() {
@@ -34,17 +36,17 @@ public class Restaurante {
 		this.nome = nome;
 	}
 
-	public BigDecimal getTaxaFreteBigDecimal() {
+	public BigDecimal getTaxaFrete() {
 		return taxaFrete;
 	}
 
-	public void setTaxaFreteBigDecimal(BigDecimal taxaFreteBigDecimal) {
-		this.taxaFrete = taxaFreteBigDecimal;
+	public void setTaxaFrete(BigDecimal taxaFrete) {
+		this.taxaFrete = taxaFrete;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		return Objects.hash(idLong);
 	}
 
 	@Override
@@ -56,9 +58,8 @@ public class Restaurante {
 		if (getClass() != obj.getClass())
 			return false;
 		Restaurante other = (Restaurante) obj;
-		return Objects.equals(id, other.id);
+		return Objects.equals(idLong, other.idLong);
 	}
 	
 	
-
 }
